3afe7ca3b67aa231faaceab906ab552d3d5f5988
mick delaney <mickdelaney@gmail.com>
2025-06-07 09:18:49 +0100

    Add Mdx Support

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import mdx from '@astrojs/mdx';
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import { SITE } from "./src/config";
@@ -9,6 +10,7 @@ import { SITE } from "./src/config";
 export default defineConfig({
   site: SITE.website,
   integrations: [
+    mdx(),
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
     }),
@@ -30,10 +32,9 @@ export default defineConfig({
   image: {
     // Used for all Markdown images; not configurable per-image
     // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
-    experimentalLayout: "responsive",
+    experimentalLayout: "constrained",
   },
   experimental: {
-    svg: true,
     responsiveImages: true,
     preserveScriptOrder: true,
   },
